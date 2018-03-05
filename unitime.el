678e94d8c877dd5c1c5ffc2d1d4ff2cbb7ab4c9a
John Herrlin <jherrlin@gmail.com>
2018-03-05 20:05:27 +0100

    Better code, some documentation

--- unitime.el
+++ unitime.el
@@ -4,15 +4,16 @@
 (require 'json)
 
 
-(defvar unitime/courses-list (list "2DV50E")
-  "Unitime courses as lists in a string.i
+(defvar unitime/courses-list (list "2DV50E" "1DV507")
+  "Unitime courses as lists in a string.
 
 Example:
 (defvar unitime/courses-list (list \"2DV50E\"))")
 
+
 (defun unitime--request (type course)
   "Get data from Unitime API."
-  (let* ((url (if (eq type 'course) "https://unitime.jherrlin.se/api/course/"
+  (let* ((url (if (eq type 'courses) "https://unitime.jherrlin.se/api/course/"
                 "https://unitime.jherrlin.se/api/lectures/"))
          (response (request
                     url
@@ -22,66 +23,52 @@ Example:
                     :headers '(("Content-Type" . "application/json"))
                     :parser 'json-read))
          (data (request-response-data response)))
-    data))
+    `(,course . ,data)))
 
-(defun unitime--courses ()
-  (map 'list (lambda (course)
-            (unitime--request 'course course))
-          unitime/courses-list))
 
-(defun unitime--lectures ()
+(defun unitime--get (type)
+  "Return a list with results from unitime--request.
+
+This function iterates over the courses and get the relavant
+data.
+
+Example call:
+(unitime--get 'courses)"
   (map 'list (lambda (course)
-               (unitime--request 'lectures course))
+               (if (eq type 'courses)
+                   (unitime--request 'courses course)
+                 (unitime--request 'lectures course)))
        unitime/courses-list))
+
 
 (defun unitime--lecture-table-row-header ()
   "Return a string with lecture headers for the orgmode table"
   (concatenate 'string
-               "|"
-               "Start datetime"
-               "|"
-               "End datetime"
-               "|"
-               "Teacher"
-               "|"
-               "Info"
-               "|"
-               "Description"
-               "|"
-               "Room name"
-               "|"
-               "Room floor"
-               "|"
-               "Room lat"
-               "|"
-               "Room lon"
-               "|"
-               ))
+               "|" "Start datetime" "|" "End datetime"
+               "|" "Teacher" "|" "Info" "|" "Description"
+               "|" "Room name" "|" "Room floor" "|" "Room lat"
+               "|" "Room lon" "|"))
+
+
+(defun unitime--get-buffer ()
+  "Returns the unitime buffer."
+  (get-buffer-create "unitime-buffer"))
 
 
 (defun unitime--parse-lecture-to-table-row-str (course)
   "Parse a lecture alist into a orgmode looking table row string."
-  (setq room (cdr (assoc 'room course)))
-  (concatenate 'string
-               "|"
-               (cdr (assoc 'start_datetime course))
-               "|"
-               (cdr (assoc 'end_datetime course))
-               "|"
-               (cdr (assoc 'teacher course))
-               "|"
-               (cdr (assoc 'info course))
-               "|"
-               (cdr (assoc 'description course))
-               "|"
-               (cdr (assoc 'name room))
-               "|"
-               (number-to-string (cdr (assoc 'floor room)))
-               "|"
-               (number-to-string (cdr (assoc 'lat room)))
-               "|"
-               (number-to-string (cdr (assoc 'lon room)))
-               "|"))
+  (let ((room (cdr (assoc 'room course))))
+    (concatenate 'string
+                 "|" (cdr (assoc 'start_datetime course))
+                 "|" (cdr (assoc 'end_datetime course))
+                 "|" (cdr (assoc 'teacher course))
+                 "|" (cdr (assoc 'info course))
+                 "|" (cdr (assoc 'description course))
+                 "|" (cdr (assoc 'name room))
+                 "|" (number-to-string (cdr (assoc 'floor room)))
+                 "|" (number-to-string (cdr (assoc 'lat room)))
+                 "|" (number-to-string (cdr (assoc 'lon room)))
+                 "|")))
 
 
 (defun unitime--insert-buffer-header ()
@@ -90,18 +77,19 @@ Example:
       (erase-buffer)
       (funcall 'org-mode)
       (insert "#+TITLE: Unitime-el\n")
-      (insert "#+AUTHOR: John Herrlin\n")
-      (insert "#+EMAIL: jherrlin@gmail.com\n")
       (insert "#+STARTUP: content\n")
       (insert "\n\n"))))
 
 
 (defun unitime--insert-lecture-table (lectures)
-  (let ((unitime-buffer (get-buffer-create "unitime-buffer")))
+  (let ((unitime-buffer (get-buffer-create "unitime-buffer"))
+        (course (car lectures))
+        (lectures (cdr lectures)))
     (with-current-buffer unitime-buffer
+      (insert (concatenate 'string "* " course "\n"))
       (insert (unitime--lecture-table-row-header))
       (insert "\n|-\n")
-      (mapc
+      (mapc ;; Iterate over lectures
        (lambda (x)
          (insert (unitime--parse-lecture-to-table-row-str x))
          (insert "\n"))
@@ -111,13 +99,14 @@ Example:
       (point-max)
       (insert "\n\n"))))
 
+
 (defun unitime ()
   (interactive)
   (unitime--insert-buffer-header)
   (mapc
    (lambda (x)
      (unitime--insert-lecture-table x))
-   (unitime--lectures))
+   (unitime--get 'lectures))
   (switch-to-buffer (get-buffer-create "unitime-buffer")))
 
 
